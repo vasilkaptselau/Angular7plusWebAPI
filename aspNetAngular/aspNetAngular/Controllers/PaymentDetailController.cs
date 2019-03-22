fdbd2e1de3a47a91f3d6f06d9db88ca0aa8eed21
Vasil Kaptselau <kopvasya@gmail.com>
2019-03-22 01:29:50 -0700

    initial

---
+++ aspNetAngular/aspNetAngular/Controllers/PaymentDetailController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using aspNetAngular.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace aspNetAngular.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly PaymentDetailsContext _context;
+
+        public PaymentDetailController(PaymentDetailsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetail
+        // [EnableCors("CorePolicy")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
+        {
+            return await _context.PaymentDetails.ToListAsync();
+        }
+
+       
+        // PUT: api/PaymentDetail/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetail([FromRoute] int id, [FromBody] PaymentDetail paymentDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != paymentDetail.OwnerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // GET: api/PaymentDetail/5
+        // [EnableCors]        // Default policy.
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetail>> GetPaymentDetail(int id)
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetail;
+        }
+
+
+        // POST: api/PaymentDetail
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
+        {
+            _context.PaymentDetails.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.OwnerId }, paymentDetail);
+        }
+
+        // DELETE: api/PaymentDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetail([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetails.Remove(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return Ok(paymentDetail);
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.PaymentDetails.Any(e => e.OwnerId == id);
+        }
+    }
+}
---
+++ aspNetAngular/aspNetAngular/Models/PaymentDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNetAngular.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int OwnerId { get; set; }
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(19)")]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CW { get; set; }
+    }
+}
